998827a2c51820e32beb4d053015def5d1daf537
tusonggao <tusonggao@163.com>
2018-12-18 23:46:52 +0800

    update file

--- 2018_12_16/compute_maidian.py
+++ 2018_12_16/compute_maidian.py
@@ -4,5 +4,8 @@ from datetime import datetime
 now = datetime.now()
 print(now.year, now.month, now.day)
 
+delta = datetime(2011, 1, 7) - datetime(2008, 6, 24, 8, 15)
+print(delta)
+
 df = pd.read_csv('df_maidian_small.csv')
 print(df.shape)
