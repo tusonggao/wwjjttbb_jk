e8bceda8939112ba15b03d1af2868fdaabce087b
tusonggao <tusonggao@163.com>
2018-12-24 19:35:17 +0800

    update file

--- 2018_12_16/compute_maidian.py
+++ 2018_12_16/compute_maidian.py
@@ -7,5 +7,8 @@ print(now.year, now.month, now.day)
 delta = datetime(2011, 1, 7) - datetime(2008, 6, 24, 8, 15)
 print(delta)
 
-df = pd.read_csv('df_maidian_small.csv')
-print(df.shape)
+df = pd.read_csv('df_maidian_small.csv', index_col=0)
+print('df.columns is ', df.columns)
+
+df2 = pd.pivot_table(df, index=['date'])
+print(df2.shape)
